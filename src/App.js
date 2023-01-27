560f7e02fa98786fbe471ae82dfd79bdc13e4496
jmjusmin <jmjusmin1412@gmail.com>
2023-01-26 21:57:14 -0800

    Move up the function to app state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import Counter from "./Counter";
 import Total from "./Total";
@@ -10,26 +11,54 @@ function App() {
     { id: 3, value: 0 },
   ];
 
+  /* 3. Render a fourth Counter component and ensure it's value is updated independently from the others. */
+  data.push({ id: 4, value: 0 });
+
   const [total, setTotal] = useState(0);
+  const [newArr, setNewArr] = useState([]);
 
-  const handleChange = (value) => {
-    setTotal(total + value);
+  useEffect(() => {
+    setNewArr(data);
+  }, []);
+
+  //1.Update the Counter component to take onIncrement and onDecrement callbacks as props and ensure they update the counter's values independently. Each callback should take a single, integer value as a parameter which is the amount to increment the counter's existing value by.
+
+  // const onDecrement = (id, value) => {
+  //   newArr.map((item) => {
+  //     if (item.id === id) item.value += value;
+  //   });
+  //   setTotal(total + value);
+  // };
+  // const onIncrement = (id, value) => {
+  //   newArr.map((item) => {
+  //     if (item.id === id) item.value += value;
+  //   });
+  //   setTotal(total + value);
+  // };
+
+  //5. Make a copy of the Counter component, saving the original so you're instructor can view it later. Then do the following :
+  //remove the onIncrement and onDecrement props from the (new) Counter component
+  //add a single onChange callback prop that takes a single integer parameter — the new value for the counter.
+  //Ensure all Counter components still update and function independently after this change.
+  const onChange = (id, updatedValue) => {
+    newArr.map((item) => {
+      if (item.id === id) item.value += updatedValue;
+    });
+    setTotal(total + updatedValue);
   };
-
   return (
     <div className="App">
       <h1>Test</h1>
-      {data.map((counter) => (
+      {newArr.map((counter) => (
         <Counter
           key={counter.id}
           id={counter.id}
           value={counter.value}
-          handleChange={handleChange}
+          // onDecrement={onDecrement}
+          // onIncrement={onIncrement}
+          onChange={onChange}
         />
       ))}
-
-      {/* 3. Render a fourth Counter component and ensure it's value is updated independently from the others. */}
-      <Counter id={4} value={0} handleChange={handleChange} />
 
       {/* 4. Create a Total component, which should display below the Counter components and always display the running total of all the Counter values. */}
       <Total total={total} />
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,38 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Counter({ id, value, handleChange }) {
-  const [count, setCount] = useState(0);
-
-  value = count;
-
-  //1.Update the Counter component to take onIncrement and onDecrement callbacks as props and ensure they update the counter's values independently. Each callback should take a single, integer value as a parameter which is the amount to increment the counter's existing value by.
-
-  const onDecrement = (id, value) => {
-    const updatedValue = value - 1;
-    setCount(updatedValue);
-    handleChange(-1);
-    console.log(`Key at ${id} : -, value: ${updatedValue}`);
-  };
-  const onIncrement = (id, value) => {
-    const updatedValue = value + 1;
-    setCount(updatedValue);
-    handleChange(1);
-    console.log(`Key at ${id} : +, value: ${updatedValue}`);
-  };
-
+function Counter({ id, value, onChange }) {
   return (
     <div className="counter">
       <b>{value}</b>
       <div className="counter-controls">
         <button
           className="button is-danger is-small"
-          onClick={() => onDecrement(id, value)}
+          onClick={() => onChange(id, -1)}
         >
           -
         </button>
         <button
           className="button is-success is-small"
-          onClick={() => onIncrement(id, value)}
+          onClick={() => onChange(id, 1)}
         >
           +
         </button>
